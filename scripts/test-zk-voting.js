b1e40f571c0bfbd7e74dc00d6ea2c4f02b6749af
yyczz <a729746954@gmail.com>
2025-03-23 13:58:45 +0800

    circuits

--- scripts/test-zk-voting.js
+++ scripts/test-zk-voting.js
@@ -16,8 +16,8 @@ function formatProofForContract(proof) {
   return {
     a: [proof.pi_a[0], proof.pi_a[1]],
     b: [
-      [proof.pi_b[0][0], proof.pi_b[0][1]],
-      [proof.pi_b[1][0], proof.pi_b[1][1]]
+      [proof.pi_b[0][1], proof.pi_b[0][0]],
+      [proof.pi_b[1][1], proof.pi_b[1][0]]
     ],
     c: [proof.pi_c[0], proof.pi_c[1]]
   };
